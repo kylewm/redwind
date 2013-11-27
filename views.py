6aa89ec7a7fef758cd28bdf0bb22511a87d1dd6b
Kyle Mahan <kyle.mahan@gmail.com>
2013-11-27 10:16:28 -0800

    remove duplicate auth code

--- views.py
+++ views.py
@@ -4,30 +4,7 @@ from functools import wraps
 
 from app import *
 from models import *
-
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    user = User.query.filter_by(login=username).first()
-    return user and user.check_password(password)
-
-def authenticate():
-    """Sends a 401 response that enables basic auth"""
-    return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
-
+from auth import requires_auth
 
 def get_posts(post_type, page, per_page):
     pagination = Post.query\
