5a55e6e25b347fd34976f9d85bc86009b2a2f56b
Kyle Mahan <kyle.mahan@gmail.com>
2013-11-28 10:25:03 -0800

    Update dependencies and fabric file

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,22 @@
 
-from fabric.api import local, settings, abort, run, cd
+from fabric.api import *
 from fabric.context_managers import prefix
 from fabric.contrib.console import confirm
+
+env.hosts = [ 'groomsman@orin.kylewm.com' ]
 
 def prepare_deploy():
     local("git add -p && git commit")
     local("git push origin master")
 
-def deploy():
-    prepare_deploy()
+def update_server():
     with cd("~/groomsman"):
-        with prefix("workon groomsman"):
+        with prefix("source venv/bin/activate"):
             run("git pull origin master")
             run("pip install -r requirements.txt")
             run("uwsgi --reload /tmp/groomsman.pid")
+
+def deploy():
+    prepare_deploy()
+    update_server()
+    
