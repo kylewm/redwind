565e728aa021699173e391ee6249ae133d388a99
Kyle Mahan <kyle.mahan@gmail.com>
2013-12-02 16:43:40 -0800

    Made pygments.css and userimg static resources

---
+++ update_static_resources.py
@@ -0,0 +1,14 @@
+import requests
+import pygments.formatters
+import shutil
+
+PYGMENTS_STYLE = 'tango'
+pygments_css = (pygments.formatters.HtmlFormatter(style=PYGMENTS_STYLE)
+                .get_style_defs('.codehilite'))
+with open('static/css/pygments.css', 'w') as f:
+    f.write(pygments_css)
+
+response = requests.get("http://www.gravatar.com/avatar/767447312a2f39bec228c3925e3edf74?s=64", stream=True)
+with open('static/img/users/kyle.jpg', 'wb') as f:
+    shutil.copyfileobj(response.raw, f)
+del response
