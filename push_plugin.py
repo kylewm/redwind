9cd72406d63d1ce3a6cd75a65131c8bde3df95ba
Kyle Mahan <kyle.mahan@gmail.com>
2013-12-04 17:56:22 -0800

    Send the all.atom feed and the articles/notes feeds to PuSH

--- push_plugin.py
+++ push_plugin.py
@@ -4,9 +4,18 @@ class PushClient:
     def __init__(self, app):
         self.app = app
 
-    def handle_new_or_edit(self, post):
-        data = { 'hub.mode' : 'publish', 
-                 'hub.url' : 'http://kylewm.com/all.atom' }
+    def publish(self, url):
+        data = { 'hub.mode' : 'publish', 'hub.url' : url }
         response = requests.post('https://pubsubhubbub.appspot.com/', data)
         if response.status_code == 204:
             self.app.logger.info('successfully sent PuSH notification')
+        else:
+            self.app.logger.warn('unexpected response from PuSH hub %s', response)
+        
+    def handle_new_or_edit(self, post):
+        self.publish('http://kylewm.com/all.atom')
+        if post.post_type=='article':
+            self.publish('http://kylewm.com/articles.atom')
+        elif post.post_type=='note':
+            self.publish('http://kylewm.com/notes.atom')
+            
