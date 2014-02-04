05f1f2c3763912e7ce4a62c9ee8a82e6fbf421c1
Kyle Mahan <kyle.mahan@gmail.com>
2014-02-04 09:10:36 -0800

    Added upload.js

---
+++ static/js/upload.js
@@ -0,0 +1,28 @@
+$(":file").change(function() {
+    $(this.files).each(function(idx, file) {
+        name = file.name;
+        size = file.size;
+        type = file.type;
+        console.log("uploading:", name, size, type);
+
+        var formData = new FormData();
+        formData.append('file', file);
+        
+        $.ajax({
+            url: '/admin/upload',
+            type: 'POST',
+            success: completeHandler,
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+    });
+});
+
+function completeHandler(response) {
+    console.log("received upload response:", response);
+    $("#uploads").append(
+        "<a href=\"" + response.path  + "\">" + response.path +  "</a><br />"
+    );
+}
