43b77eab65a0801f170a5c94befee86d54dd6843
Kyle Mahan <kyle.mahan@gmail.com>
2014-03-08 11:27:35 -0800

    Add "See Original" link for facebook (or at least try)

--- facebook_plugin.py
+++ facebook_plugin.py
@@ -31,12 +31,11 @@ def authorize_facebook():
 
 
 class FacebookClient:
-
     def __init__(self, app):
         self.app = app
             
     def handle_new_or_edit(self, post):
         graph = facebook.GraphAPI(post.author.facebook_access_token)
-        response = graph.put_object("me", "feed", name=post.title, message=post.content, link=post.repost_source)
+        response = graph.put_object("me", "feed", name=post.title, message=post.content, link=post.repost_source, actions=["See Original", post.permalink_url])
         post.facebook_post_id = response['id']
 
