5d7de4386a4f2039032eab5adc9ed1cc0dd48c86
Kyle Mahan <kyle.mahan@gmail.com>
2014-03-13 20:31:54 -0700

    added a little intelligence for PuSHing on updates

--- push_plugin.py
+++ push_plugin.py
@@ -1,21 +1,24 @@
 import requests
 
+
 class PushClient:
     def __init__(self, app):
         self.app = app
 
     def publish(self, url):
-        data = { 'hub.mode' : 'publish', 'hub.url' : url }
+        self.app.logger.debug("sending PuSH notification to %s", url)
+        data = {'hub.mode': 'publish', 'hub.url': url}
         response = requests.post('https://pubsubhubbub.appspot.com/', data)
         if response.status_code == 204:
             self.app.logger.info('successfully sent PuSH notification')
         else:
-            self.app.logger.warn('unexpected response from PuSH hub %s', response)
-        
+            self.app.logger.warn('unexpected response from PuSH hub %s',
+                                 response)
+
     def handle_new_or_edit(self, post):
-        self.publish('http://kylewm.com/all.atom')
-        if post.post_type=='article':
+        if post.post_type in ('article', 'note', 'share'):
+            self.publish('http://kylewm.com/all.atom')
+        if post.post_type == 'article':
             self.publish('http://kylewm.com/articles.atom')
-        elif post.post_type=='note':
+        elif post.post_type == 'note':
             self.publish('http://kylewm.com/notes.atom')
-            
