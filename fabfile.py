1e3f86b4a3ef5af6e05913d1c125c87310958917
Kyle Mahan <kyle.mahan@gmail.com>
2014-03-17 06:32:16 -0700

    rename stuff to red-wind

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import local, prefix, cd, run, env
 
 
-env.hosts = ['groomsman@orin.kylewm.com']
+env.hosts = ['orin.kylewm.com']
 
 
 def commit():
@@ -14,15 +14,15 @@ def push():
 
 
 def pull():
-    with cd("~/groomsman"):
+    with cd("~/red-wind"):
         run("git pull origin master")
 
 
 def restart():
-    with cd("~/groomsman"):
+    with cd("~/red-wind"):
         with prefix("source venv/bin/activate"):
             run("pip install -r requirements.txt")
-            run("uwsgi --reload /tmp/groomsman.pid")
+            run("uwsgi --reload /tmp/red-wind.pid")
 
 
 def deploy():
