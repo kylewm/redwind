3e58576d0adf21a4dad6e70d5e356765a002b7d3
Kyle Mahan <kyle.mahan@gmail.com>
2014-03-24 20:17:06 +0000

    Better discovery of authorship h-card

--- hentry_parser.py
+++ hentry_parser.py
@@ -11,11 +11,35 @@ bleach.ALLOWED_ATTRIBUTES.update({
     'img': ['src', 'alt', 'title']
 })
 
-Author = namedtuple('Author', ['name', 'url', 'photo'])
-Reference = namedtuple('Reference', ['url', 'reftype'])
-Entry = namedtuple('Entry', ['author', 'permalink', 'pub_date',
-                             'references', 'title', 'content'])
+class Author:
+    def __init__(self, name, url, photo):
+        self.name = name
+        self.url = url
+        self.photo = photo
 
+    def __repr__(self):
+        return "Author[name={}, url={}, photo={}]".format(self.name, self.url, self.photo)
+        
+class Reference:
+    def __init__(self, url, reftype):
+        self.url = url
+        self.reftype = reftype
+
+    def __repr__(self):
+        return "Reference[url={}, reftype={}]".format(self.url, self.reftype)
+
+class Entry:
+    def __init__(self, author, permalink, pub_date,
+                 references, title, content):
+        self.author = author
+        self.permalink = permalink
+        self.pub_date = pub_date
+        self.references = references
+        self.title = title
+        self.content = content
+
+    def __repr__(self):
+        return "Entry[author={}, permalink={}, pub_date={}, references={}, title={}, content={}]".format(self.author, self.permalink, self.pub_date, self.references, self.title, self.content[:100])
 
 def parse(txt, source):
     def parse_references(objs, reftype):
@@ -28,17 +52,16 @@ def parse(txt, source):
                          in obj.get('properties', {}).get('url', [])]
         return refs
 
-    def parse_author(objs):
-        for obj in objs:
-            if isinstance(obj, str):
-                return Author(obj, None, None)
-            else:
-                names = obj['properties'].get('name')
-                photos = obj['properties'].get('photo')
-                urls = obj['properties'].get('url')
-                return Author(names and names[0],
-                              urls and urls[0],
-                              photos and photos[0],)
+    def parse_author(obj):
+        if isinstance(obj, str):
+            return Author(obj, None, None)
+        else:
+            names = obj['properties'].get('name')
+            photos = obj['properties'].get('photo')
+            urls = obj['properties'].get('url')
+            return Author(names and names[0],
+                          urls and urls[0],
+                          photos and photos[0])
 
     p = Parser(doc=txt, url=source)
     d = p.to_dict()
@@ -52,6 +75,7 @@ def parse(txt, source):
         elif rel in ('repost', 'repost-of'):
             references.append(Reference(rel_url, 'repost'))
 
+    entry = None
     for item in d['items']:
         if 'h-entry' in item['type']:
 
@@ -70,7 +94,7 @@ def parse(txt, source):
 
             date_strs = hentry['properties'].get('published')
             pub_date = date_strs and parsedate(' '.join(date_strs))
-            if pub_date:
+            if pub_date and pub_date.tzinfo:
                 pub_date = pub_date.astimezone(pytz.utc)
 
             content_html = ''.join(content['html'].strip() for content
@@ -85,10 +109,42 @@ def parse(txt, source):
             if title == content_value:
                 title = None
 
-            author = parse_author(
-                hentry['properties'].get('author', []))
-            return Entry(author, permalink, pub_date, references, title,
-                         content_html)
+            author = None
+            for obj in hentry['properties'].get('author', []):
+                author = parse_author(obj)
+                break
+                
+            entry = Entry(author, permalink, pub_date, references, title,
+                          content_html)
+            break
+
+    hcards = [item for item in d['items'] if 'h-card' in item['type']]
+       
+    if entry and not entry.author:
+        for item in hcards:
+            urls = item['properties'].get('url', [])
+            if source in urls:
+                entry.author = parse_author(item)
+                break
+                        
+    if entry and not entry.author:
+        rel_mes = d["rels"].get("me", [])
+        for item in hcards:
+            urls = item['properties'].get('url', [])
+            if any(url in rel_mes for url in urls):
+                entry.author = parse_author(item)
+                break
+
+    if entry and not entry.author:
+        for item in hcards:
+            if 'url' in item['properties']:
+                entry.author = parse_author(item)
+                break
+
+    if entry and not entry.author and hcards:
+        entry.author = parse_author(hcards[0])
+
+    return entry
 
 
 if __name__ == '__main__':
@@ -106,11 +162,13 @@ if __name__ == '__main__':
 
         'https://brid-gy.appspot.com/comment/twitter/kyle_wm/443763597160636417/443787536108761088',
 
-        'https://snarfed.org/2014-03-10_re-kyle-mahan-5'
+        'https://snarfed.org/2014-03-10_re-kyle-mahan-5',
+
+        'http://tommorris.org/posts/2550'
     ]
 
     for url in urls:
         print("parsing url", url)
         txt = requests.get(url).content
-        print(parse(txt))
+        print(parse(txt, url))
         print()
--- twitter_plugin.py
+++ twitter_plugin.py
@@ -134,7 +134,7 @@ class TwitterClient:
 
             pub_date = datetime.strptime(status_data['created_at'],
                                          '%a %b %d %H:%M:%S %z %Y')
-            if pub_date:
+            if pub_date and pub_date.tzinfo:
                 pub_date = pub_date.astimezone(pytz.utc)
             real_name = status_data['user']['name']
             screen_name = status_data['user']['screen_name']
