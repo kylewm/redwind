237f8dd17fa241f5c642593a87cacb8905734547
Kyle Mahan <kyle.mahan@gmail.com>
2014-03-25 08:06:41 -0700

    GPL all the things!

--- views.py
+++ views.py
@@ -1,3 +1,20 @@
+# Copyright © 2013, 2014 Kyle Mahan
+# This file is part of Red Wind.
+#
+# Red Wind is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Red Wind is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Red Wind.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from app import app, db
 from models import Post, Mention, ReplyContext, LikeContext, ShareContext
 from auth import load_user
