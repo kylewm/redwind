285fb74cde2f6437c9b8e6076fb2212705cf1e63
Kyle Mahan <kyle.mahan@gmail.com>
2014-05-21 09:17:32 -0700

    Added image upload previews

--- redwind/static/js/editor.js
+++ redwind/static/js/editor.js
@@ -1,42 +1,15 @@
 (function() {
 
-    function clearResults() {
-        $('#result').empty();
+    function clearUploads() {
+        $('#uploads').empty();
     }
 
-    function appendResult(str) {
-        $('#result').append("<li>" + str + "</li>");
-    }
-
-    function uploadCompleteHandler(data) {
-        appendResult("finished uploading to " + data.original);
-        appendResult("<ul><li>small " + data.small
-                     + "</li><li>medium " + data.medium
-                     + "</li><li>large " + data.large + "</li></ul>");
-
-        var content_text_area = $("#content");
-        var content_format = $("#content_format").val();
-
-        content_text_area.val( content_text_area.val() + '[![](' + data.medium + ')](' + data.original + ')');
-    }
-
-    function uploadErrorHandler(data, status, errorThrown) {
-        appendResult("upload failed with " + status + ", " + errorThrown);
-    }
-
-    function appendResult(str) {
-        $('#result').append("<li>" + str + "</li>");
+    function appendUpload(str) {
+        $('#uploads').append("<li>" + str + "</li>");
     }
 
     /* register events */
     $(document).ready(function() {
-        $('#uploads_link').click(function(event) {
-            event.preventDefault();
-            var left = (screen.width-100) / 2;
-            var top = (screen.height-100) / 2;
-
-            window.open('/admin/uploads', 'width=100,height=100,top='+top+',left='+left);
-        });
 
         $("#get_coords_button").click(function() {
             navigator.geolocation.getCurrentPosition(function(position) {
@@ -47,23 +20,14 @@
         });
 
         $("#image_upload_button").change(function() {
-            var file = this.files[0];
-            $('#result').append("<li>uploading file " + file.name + "</li>");
-
-            if (undefined != file) {
-                var formData = new FormData();
-                formData.append('file', file);
-
-                $.ajax({
-                    url: '/api/upload_image',
-                    type: 'POST',
-                    success: uploadCompleteHandler,
-                    error: uploadErrorHandler,
-                    data: formData,
-                    cache: false,
-                    contentType: false,
-                    processData: false
-                });
+            $('#uploads').empty();
+            for (var ii = 0 ; ii < this.files.length ; ii++) {
+                var file = this.files[ii];
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#uploads').append('<ul><img style="max-width: 75px; max-height: 75px;" src="' + e.target.result + '"/> ' + file.name + '</ul>')
+                };
+                reader.readAsDataURL(file);
             }
         });
     });
