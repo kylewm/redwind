188837d38e0fd447bcd1550cb5c48b2e789a4635
Kyle Mahan <kyle.mahan@gmail.com>
2014-05-22 09:42:58 -0700

    Clean up posting interface

--- redwind/static/js/editor.js
+++ redwind/static/js/editor.js
@@ -1,31 +1,69 @@
 (function() {
 
-    function clearUploads() {
-        $('#uploads').empty();
-    }
-
-    function appendUpload(str) {
-        $('#uploads').append("<li>" + str + "</li>");
+    function add_img_link(file) {
+        $('#content').val(
+            $('#content').val() + '\n![/path/to/post/files/' + file.name + '](' + file.name + ')');
     }
 
     /* register events */
     $(document).ready(function() {
+        $('#syndication_textarea').css('display','none');
+        $('#audience_textarea').css('display', 'none');
 
-        $("#get_coords_button").click(function() {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                console.log(position);
-                $("#latitude").val(position.coords.latitude.toFixed(3));
-                $("#longitude").val(position.coords.longitude.toFixed(3));
-            });
+        $('#syndication_expander').click(function(){
+            var textarea = $('#syndication_textarea');
+            var closed = textarea.css('display') == 'none';
+            if (closed) {
+                textarea.css('display', '');
+            }else {
+                textarea.css('display', 'none');
+            }
+            $(this).toggleClass('fa-plus-square-o', !closed);
+            $(this).toggleClass('fa-minus-square-o', closed);
         });
 
-        $("#image_upload_button").change(function() {
+        $('#audience_expander').click(function(){
+            var textarea = $('#audience_textarea');
+            var closed = textarea.css('display') == 'none';
+            if (closed) {
+                textarea.css('display', '');
+            }else {
+                textarea.css('display', 'none');
+            }
+            $(this).toggleClass('fa-plus-square-o', !closed);
+            $(this).toggleClass('fa-minus-square-o', closed);
+        });
+
+
+        $('#get_coords_button').change(function() {
+            console.log('changed');
+            console.log(this);
+            console.log(this.checked);
+            if (this.checked) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    console.log(position);
+                    $('#latitude').val(position.coords.latitude.toFixed(3));
+                    $('#longitude').val(position.coords.longitude.toFixed(3));
+                });
+            }
+            else {
+                $('#latitude').val('');
+                $('#longitude').val('');
+            }
+        });
+
+        $('#image_upload_button').change(function() {
             $('#uploads').empty();
             for (var ii = 0 ; ii < this.files.length ; ii++) {
                 var file = this.files[ii];
                 var reader = new FileReader();
                 reader.onload = function (e) {
-                    $('#uploads').append('<ul><img style="max-width: 75px; max-height: 75px;" src="' + e.target.result + '"/> ' + file.name + '</ul>')
+                    var link = $('<a>');
+                    link.append('<img style="max-width: 75px; max-height: 75px;" src="' + e.target.result + '"/>' + file.name);
+                    $('#uploads').append($('<ul>').append(link));
+                    link.click(function(){
+                        add_img_link(file);
+                    });
                 };
                 reader.readAsDataURL(file);
             }
