8672349ed90ceadc1cb0e203315859cbf1c8012e
Kyle Mahan <kyle.mahan@gmail.com>
2015-05-27 19:00:31 +0000

    update install.py to use application factories. Fixes #49

--- install.py
+++ install.py
@@ -1,74 +1,78 @@
 #!/usr/bin/env python
 
-from redwind import app, db, models
+from redwind import create_app, models
+from redwind.extensions import db
+from flask import current_app
 
-print('''\
-Welcome to the Red Wind installation script. This should only be used for
-to initialize a brand new database. Running this script against an existing
-database could destroy your data!''')
+app = create_app()
+with app.app_context():
+    print('''\
+    Welcome to the Red Wind installation script. This should only be used for
+    to initialize a brand new database. Running this script against an existing
+    database could destroy your data!''')
 
-author_domain = input('Domain name (no schema): ').strip()
-twitter_username = input('Your Twitter username: ').strip()
-github_username = input('Your GitHub username: ').strip()
-
-
-print('creating database tables for database', app.config['SQLALCHEMY_DATABASE_URI'])
-db.create_all()
-print('done creating database tables')
-
-bio = '''
-<div class="p-author h-card">
-<a class="p-name u-url" href="/">New User</a> is a brand new Red Wind user!
-Visit <a href="/settings">Settings</a> to edit your bio.
-<ul>
-<li><a rel="me" href="https://twitter.com/{}">Twitter</a></li>
-<li><a rel="me" href="https://github.com/{}">GitHub</a></li>
-</ul>
-</div>
-'''.format(twitter_username.lstrip('@'),
-           github_username)
+    author_domain = input('Domain name (no schema): ').strip()
+    twitter_username = input('Your Twitter username: ').strip()
+    github_username = input('Your GitHub username: ').strip()
 
 
-print('setting default settings')
+    print('creating database tables for database', current_app.config['SQLALCHEMY_DATABASE_URI'])
+    db.create_all()
+    print('done creating database tables')
 
-defaults = [
-    ('Author Name',                ''),
-    ('Author Image',               ''),
-    ('Author Domain',              author_domain),
-    ('Author Bio',                 bio),
-    ('Site Title',                 ''),
-    ('Site URL',                   ''),
-    ('Shortener URL',              None),
-    ('Push Hub',                   ''),
-    ('Posts Per Page',             15),
-    ('Twitter API Key',            ''),
-    ('Twitter API Secret',         ''),
-    ('Twitter OAuth Token',        ''),
-    ('Twitter OAuth Token Secret', ''),
-    ('Facebook App ID',            ''),
-    ('Facebook App Secret',        ''),
-    ('Facebook Access Token',      ''),
-    ('WordPress Client ID',        ''),
-    ('WordPress Client Secret',    ''),
-    ('WordPress Access Token',     ''),
-    ('Instagram Client ID',        ''),
-    ('Instagram Client Secret',    ''),
-    ('Instagram Access Token',     ''),
-    ('PGP Key URL',                ''),
-    ('Avatar Prefix',              'nobody'),
-    ('Avatar Suffix',              'png'),
-    ('Timezone',                   'America/Los_Angeles'),
-]
+    bio = '''
+    <div class="p-author h-card">
+    <a class="p-name u-url" href="/">New User</a> is a brand new Red Wind user!
+    Visit <a href="/settings">Settings</a> to edit your bio.
+    <ul>
+    <li><a rel="me" href="https://twitter.com/{}">Twitter</a></li>
+    <li><a rel="me" href="https://github.com/{}">GitHub</a></li>
+    </ul>
+    </div>
+    '''.format(twitter_username.lstrip('@'),
+               github_username)
 
-for name, default in defaults:
-    key = name.lower().replace(' ', '_')
-    s = models.Setting.query.get(key)
-    if not s:
-        s = models.Setting()
-        s.key = key
-        s.name = name
-        s.value = default
-        db.session.add(s)
-db.session.commit()
 
-print('finished setting default settings')
+    print('setting default settings')
+
+    defaults = [
+        ('Author Name',                ''),
+        ('Author Image',               ''),
+        ('Author Domain',              author_domain),
+        ('Author Bio',                 bio),
+        ('Site Title',                 ''),
+        ('Site URL',                   ''),
+        ('Shortener URL',              None),
+        ('Push Hub',                   ''),
+        ('Posts Per Page',             15),
+        ('Twitter API Key',            ''),
+        ('Twitter API Secret',         ''),
+        ('Twitter OAuth Token',        ''),
+        ('Twitter OAuth Token Secret', ''),
+        ('Facebook App ID',            ''),
+        ('Facebook App Secret',        ''),
+        ('Facebook Access Token',      ''),
+        ('WordPress Client ID',        ''),
+        ('WordPress Client Secret',    ''),
+        ('WordPress Access Token',     ''),
+        ('Instagram Client ID',        ''),
+        ('Instagram Client Secret',    ''),
+        ('Instagram Access Token',     ''),
+        ('PGP Key URL',                ''),
+        ('Avatar Prefix',              'nobody'),
+        ('Avatar Suffix',              'png'),
+        ('Timezone',                   'America/Los_Angeles'),
+    ]
+
+    for name, default in defaults:
+        key = name.lower().replace(' ', '_')
+        s = models.Setting.query.get(key)
+        if not s:
+            s = models.Setting()
+            s.key = key
+            s.name = name
+            s.value = default
+            db.session.add(s)
+    db.session.commit()
+
+    print('finished setting default settings')
